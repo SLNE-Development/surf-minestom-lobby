132c691eba2ce78f6c7849d9a100d462a939ded9
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-08-19 19:50:31 +0200

    🔧 chore(build): comment out content creator dependency in build.gradle.kts until surf-social is ready

--- surf-minestom-lobby-server/build.gradle.kts
+++ surf-minestom-lobby-server/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     runtimeDownload(libs.surf.queue.minestom)
     runtimeDownload(libs.surf.playtime.minestom)
     runtimeDownload(libs.surf.jumppad.minestom)
-    runtimeDownload(libs.surf.content.creator.minestom)
+//    runtimeDownload(libs.surf.content.creator.minestom)
     runtimeDownload(libs.surf.friends.minestom)
 //    runtimeDownload(libs.surf.lobby.minestom)
 
